0e608bf46f2dcfe0688fcedeac07b9885b41d35a
alejandroiv <alejandroiv177771@gmail.com>
2025-06-26 11:12:58 -0300

    cree el modelo de la BD dragonball

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config(); //lee y carga las variables del .env
 
-//este es como el  "molde" de la BD, aca va lo del .env
+//con esto se logra la conecion de la BD, aca va lo del .env
 const conecion_bd = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
